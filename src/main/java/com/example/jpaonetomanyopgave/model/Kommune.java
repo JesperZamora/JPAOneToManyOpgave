6a9cb322e52db7fb1d8e09d5cf19f61dfb8d3f04
JesperZamora <jesp78m6@stud.kea.dk>
2023-09-01 22:12:12 +0200

    setup of Region.java and Kommune.java + app-properties

--- src/main/java/com/example/jpaonetomanyopgave/model/Kommune.java
+++ src/main/java/com/example/jpaonetomanyopgave/model/Kommune.java
@@ -1,4 +1,49 @@
 package com.example.jpaonetomanyopgave.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "kommuner")
 public class Kommune {
+    @Id @Column(name = "k_code")
+    private String kode;
+    @Column(name = "k_name")
+    private String navn;
+    @Column(name = "k_href")
+    private String href;
+
+    @ManyToOne @JoinColumn(name = "k_region_fk", referencedColumnName = "r_code")
+    private Region region;
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    public Region getRegion() {
+        return region;
+    }
+
+    public void setRegion(Region region) {
+        this.region = region;
+    }
 }
--- src/main/java/com/example/jpaonetomanyopgave/model/Region.java
+++ src/main/java/com/example/jpaonetomanyopgave/model/Region.java
@@ -1,4 +1,53 @@
 package com.example.jpaonetomanyopgave.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "regioner")
 public class Region {
+    @Id @Column(name = "r_kode")
+    private String kode;
+    @Column(name = "r_name")
+    private String navn;
+    @Column(name = "r_href")
+    private String href;
+
+    // CascadeType.All part of assignment 2.
+    @OneToMany(mappedBy = "region", cascade = CascadeType.ALL) @JsonBackReference
+    private Set<Kommune> kommuneSet;
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    public Set<Kommune> getKommuneSet() {
+        return kommuneSet;
+    }
+
+    public void setKommuneSet(Set<Kommune> kommuneSet) {
+        this.kommuneSet = kommuneSet;
+    }
 }
